89f291b37582181820baf5dc7d8e84a3fe2f480c
Pawel Jelonek <pawel.jelonek.krakow@gmail.com>
2024-08-10 19:30:13 +0200

    Changing twitch config

--- apps/twitch-db-async-fetcher/src/config/twitchApiConfig.ts
+++ apps/twitch-db-async-fetcher/src/config/twitchApiConfig.ts
@@ -1,7 +1,7 @@
 export const config = {
-    maxRequests: 1, // Maximum number of requests to be made during one batch
+    maxRequests: 10, // Maximum number of requests to be made during one batch
     minViewerCount: 5, // Minimuym number of viewers to be included
     language: 'pl', // Language filter for streams
-    first: 1, // Number of streams to fetch per request
+    first: 100, // Number of streams to fetch per request
     directory: '/twitchDir'
 };
