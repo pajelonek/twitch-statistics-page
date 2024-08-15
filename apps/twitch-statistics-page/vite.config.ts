aed758f216afb91b279bcce6902491a16e82a22b
Pawel Jelonek <48658611+pajelonek@users.noreply.github.com>
2024-08-15 18:04:11 +0200

    Feature/poc reading file on pages (#7)
    * Adding changes to actions
    
    * POC reading files on pages

--- apps/twitch-statistics-page/vite.config.ts
+++ apps/twitch-statistics-page/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/twitch-statistics-page',
+  publicDir: '/public/',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     react() 
   ],
