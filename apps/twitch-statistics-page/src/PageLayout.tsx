203aa4cf105942ba7246744e815cb04ca9d03b44
Pawel Jelonek <48658611+pajelonek@users.noreply.github.com>
2024-08-15 16:26:25 +0200

    Feature POC of reading from file
    * Adding changes to actions
    
    * Adding POC of reading from json

--- apps/twitch-statistics-page/src/PageLayout.tsx
+++ apps/twitch-statistics-page/src/PageLayout.tsx
@@ -1,9 +1,31 @@
-import './App.css';
 import AppBar from '@mui/material/AppBar';
-import { Box, Toolbar, IconButton, Typography } from '@mui/material';
+import { Box, Toolbar, IconButton, Typography, Card } from '@mui/material';
 import Menu from '@mui/icons-material/Menu';
+import { useEffect, useState } from 'react';
 
 function PageLayout() {
+
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    fetch('twitch-statistics-page/data/test/data.json'
+      ,{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      }
+      )
+        .then(function(response){
+          console.log(response)
+          return response.json();
+        })
+        .then(function(myJson) {
+          setData(myJson.test);
+        });
+  }, []);
+
+
   return (
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
@@ -24,6 +46,7 @@ function PageLayout() {
           </Toolbar>
         </AppBar>
       </Box>
+      <Card>TEST + {data} </Card>
     </div>
   );
 }
