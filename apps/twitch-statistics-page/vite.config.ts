55865cd0eb2155ad6130fbdba2f3b3991924fdc5
Pawel Jelonek <pawel.jelonek.krakow@gmail.com>
2024-08-16 21:36:52 +0200

    Reworking vite config

--- apps/twitch-statistics-page/vite.config.ts
+++ apps/twitch-statistics-page/vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/twitch-statistics-page',
-  publicDir: '/public/',
-  build: {
-    outDir: 'dist'
-  },
   plugins: [
     react() 
   ],
