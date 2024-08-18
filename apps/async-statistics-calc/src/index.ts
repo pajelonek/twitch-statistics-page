e23993713a06c7cedf1b3f1adc0d2749e6ec668f
Pawel Jelonek <pawel.jelonek.krakow@gmail.com>
2024-08-18 19:20:22 +0200

    Adding reading files in async calculator

--- apps/async-statistics-calc/src/index.ts
+++ apps/async-statistics-calc/src/index.ts
@@ -1,5 +1,44 @@
-function main() {
-    console.log("Calculating statistics...");
+import * as fs from 'fs';
+import * as path from 'path';
 
+async function main() {
+    console.log("Calculating statistics...");
+    const directoryPath = path.join(__dirname, './test_data');
+    readAllFilesInDirectory(directoryPath);
     console.log("Task completed!");
 }
+
+async function readAllFilesInDirectory(directoryPath: string): Promise<void> {
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.error("Error reading directory:", err);
+            return;
+        }
+
+        files.forEach(file => {
+            const filePath = path.join(directoryPath, file);
+
+            fs.stat(filePath, (err, stats) => {
+                if (err) {
+                    console.error("Error getting file stats:", err);
+                    return;
+                }
+
+                if (stats.isFile()) {
+                    fs.readFile(filePath, 'utf8', (err, data) => {
+                        if (err) {
+                            console.error("Error reading file:", err);
+                            return;
+                        }
+                        console.log(`File: ${file}`);
+                        console.log(data);
+                        console.log('-----------------------------------');
+                    });
+                } else if (stats.isDirectory()) {
+                    readAllFilesInDirectory(filePath);
+                }
+            });
+        });
+    });
+}
+main();
