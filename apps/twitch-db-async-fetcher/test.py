978da62f7d02c0f2464b840da8f79501fb7847cd
Pawel Jelonek <pawel.jelonek.krakow@gmail.com>
2024-09-01 17:07:22 +0200

    Adding more flow

--- apps/twitch-db-async-fetcher/test.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup 
-import cloudscraper 
- 
-url = "https://kick.com/stream/livestreams/pl?limit=31&subcategory=&sort=desc&strict=true" 
-scraper = cloudscraper.create_scraper() 
-info = scraper.get(url) 
- 
-print(info.status_code)
