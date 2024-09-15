0b9084d39c119f87660bf6b04bdacf7d3e7431d7
Pawel Jelonek <pawel.jelonek.krakow@gmail.com>
2024-09-15 20:59:17 +0200

    Adding test table

--- apps/twitch-statistics-page/src/PageLayout.tsx
+++ apps/twitch-statistics-page/src/PageLayout.tsx
@@ -1,14 +1,15 @@
 import AppBar from '@mui/material/AppBar';
-import { Box, Toolbar, IconButton, Typography, Card } from '@mui/material';
+import { Box, Toolbar, IconButton, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableFooter, TablePagination } from '@mui/material';
 import Menu from '@mui/icons-material/Menu';
 import { useEffect, useState } from 'react';
+import TablePaginationActions from '@mui/material/TablePagination/TablePaginationActions';
 
 function PageLayout() {
 
   const [data, setData] = useState<any>(null);
   const resourceUrl = import.meta.env.VITE_RESOURCE_URL;
   useEffect(() => {
-    fetch(resourceUrl + 'data/test/data.json'
+    fetch(resourceUrl + 'data/test/summary.json'
       ,{
         headers : {
           'Content-Type': 'application/json',
@@ -17,14 +18,13 @@ function PageLayout() {
       }
       )
         .then(function(response){
-          console.log(response)
           return response.json();
         })
         .then(function(myJson) {
-          setData(myJson.test);
+          setData(myJson);
         });
   }, []);
-
+  console.log("data: ", data)
   return (
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
@@ -45,7 +45,65 @@ function PageLayout() {
           </Toolbar>
         </AppBar>
       </Box>
-      <Card>TEST + {data} </Card>
+      {data && data.length > 0 && (
+            <TableContainer >
+              <Table sx={{ maxWidth: 650, border: 1 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>WatchHours</TableCell>
+                    <TableCell>AvgViewers</TableCell>
+                    <TableCell>PeakViewers</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.map((streamer: any) => (
+                    <>
+                    <TableRow 
+                    sx={{ border: 1 }}
+                      key={streamer.streamerLogin}
+                    >
+                      <TableCell component="th" scope="row">
+                        {streamer.streamerLogin}
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {streamer.statistics.watchHours}
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {streamer.statistics.avgViewers}
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {streamer.statistics.peakViewers}
+                      </TableCell>
+                    </TableRow>
+                    </>
+                  ))}
+                </TableBody>
+              </Table>
+              <TableFooter>
+            <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                  colSpan={4}
+                  count={data.length}
+                  rowsPerPage={25}
+                  page={0}
+                  slotProps={{
+                    select: {
+                      inputProps: {
+                        'aria-label': 'rows per page',
+                      },
+                      native: true,
+                    },
+                  }}
+                  onPageChange={() => {}}
+                  onRowsPerPageChange={() => {}}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
+          </TableContainer>
+      )}
     </div>
   );
 }
